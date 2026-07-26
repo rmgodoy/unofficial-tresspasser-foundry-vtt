7ec471555323f8eef7d9855ee77e2ecaaa3b8f37
Rodrigo Godoy <godoyr888@gmail.com>
2026-07-26 09:08:52 -0300

    undo move button showing fix

--- module/hud/token-hud.mjs
+++ module/hud/token-hud.mjs
@@ -118,7 +118,7 @@ export class TrespasserTokenHUD extends HandlebarsApplicationMixin(ApplicationV2
             canHelp:        (ap >= 1 || !restrictAPF) && (!restrictHUD || !usedActions.has("help")) && this._getNearbyAllies().length > 0,
             canMove:        canMove,
             moveBtnLabel:   moveBtnLabel,
-            canUndo:        moveActionMovements.length > 0,
+            canUndo:        moveActionTaken && moveActionMovements.length > 0,
             canPrevail:     (ap >= 1 || !restrictAPF) && (!restrictHUD || !usedActions.has("prevail")) && states.length > 0,
             canAttemptDeed: (ap >= 1 || !restrictAPF) && (!restrictHUD || !usedActions.has("attempt-deed")) && deeds.length > 0 && (!restrictAPF || !usedActions.has("maneuver") || focus >= 2),
             canUseConcoction: (ap >= 1 || !restrictAPF) && concoctions.length > 0 && (!restrictHUD || !usedActions.has("use-concoction")),
